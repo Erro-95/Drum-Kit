396cc99ba6f8e9485ce3840e44c9e259d47a57c2
Edwin Rodriguez <RodriguezEdwin1324@gmail.com>
2020-07-19 02:21:52 -0400

    Attached sound to buttons

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 /*jshint esversion: 6 */
 
 var btns = document.querySelectorAll('.transitionalBtns');
+var sound = document.querySelectorAll('audio');
 
 // Create a "global-level" event listener in which will initiate upon a "keydown".
 // Function will run upon the correct key being pressed.
-// Function will add the "playing" class to the corresponding button/key pressed.
+// Function will add the "playing" class to the corresponding button/key pressed
+// as well as play the corressponding sound with the key press.
 
 window.addEventListener('keydown', (e) => {
-	var key = e.which;
+	var keyPress = e.which;
 
-	btns.forEach((btn) => {
-		if (key === parseInt(btn.dataset.key)) {
-			btn.classList.add('playing');
+	for (let i = 0; i < sound.length; i++) {
+		if (keyPress === parseInt(btns[i].dataset.key)) {
+			// Adding the "playing" class to the current btn within the node iteration.
+			btns[i].classList.add('playing');
+			// By setting the currentTime to 0, we are  resetting the audio.
+			//This lets us stop and reset the audio if the key is pressed multiple times.
+			sound[i].currentTime = 0;
+			// After ressetting the audio to the start, it is being set to play again.
+			sound[i].play();
 		}
-	});
+	}
 });
 
 // Create "Click" Event Listeners on all buttons and run function to add class of "playing" to ClassList,
-// Therefore attaching the transition animation to the element.
+// Along with the corressponding audio elemeent.
 
-btns.forEach((btn) =>
-	btn.addEventListener('click', (e) => {
-		btn.classList.add('playing');
-	})
-);
+btns.forEach((btn) => {
+	btn.addEventListener('click', addPlayingClass);
+});
+
+function addPlayingClass () {
+	this.classList.add('playing');
+	sound[parseInt(this.dataset.audio)].currentTime = 0;
+	sound[parseInt(this.dataset.audio)].play();
+}
 
 // Create a function that will remove the "playing" class from the current elemrnt.
 
-function removePlayingClass (e) {
+function removePlayingClass () {
 	this.classList.remove('playing');
 }
 
